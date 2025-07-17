1c8467cfe610429ddcae8c3839c83884d1425dbf
Jatin Sharma <jatinsharma6697@gmail.com>
2025-07-17 18:45:58 +0530

    added project

---
+++ Test_2025_Project/src/main/java/Test_Package/Test1.java
@@ -0,0 +1,20 @@
+package Test_Package;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Test1 {
+	
+	@Test
+	public void a() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://maven.apache.org/download.cgi");
+		
+	}
+
+}
